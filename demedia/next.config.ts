17806a0f0b5907415a3ce72a4691522f1684a7ab
Youssif <youssifdeveloperr999@gmail.com>
2025-09-16 23:02:49 +0300

    Enabled Next.js standalone mode

--- demedia/next.config.ts
+++ demedia/next.config.ts
@@ -7,6 +7,8 @@ const nextConfig: NextConfig = {
         // Allow production builds to succeed even if ESLint finds errors
         ignoreDuringBuilds: true,
     },
+    // Enable standalone build output
+    output: "standalone",
 };
 
 export default nextConfig;
